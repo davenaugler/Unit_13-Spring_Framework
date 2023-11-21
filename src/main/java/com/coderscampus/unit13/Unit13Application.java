391483e38acef94db0cd2d54df6d1a1589ba92d8
Dave Naugler <davenaugler@gmail.com>
2023-11-21 16:19:39 -0700

    🚀 Initial commit

---
+++ src/main/java/com/coderscampus/unit13/Unit13Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.unit13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit13Application.class, args);
+	}
+
+}
