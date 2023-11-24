3b6d78d24ae370d5a3fb6db547da9bd483575211
Dave Naugler <davenaugler@gmail.com>
2023-11-24 11:12:05 -0700

    Added File Controller

--- src/main/java/com/coderscampus/unit13/web/FileController.java
+++ src/main/java/com/coderscampus/unit13/web/FileController.java
@@ -3,6 +3,7 @@ package com.coderscampus.unit13.web;
 import com.coderscampus.unit13.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -14,9 +15,22 @@ public class FileController  {
     @Autowired
     private FileService fileService;
 
-    @GetMapping()
+    // GetMapping() listens for GET requests related to the URL path
+    // GetMapping() is what we use to listen for a request on a certain path
+    //   and direct it to a certain method
+    @GetMapping("/read-lines")
     public List<String> readLines() throws IOException {
         return fileService.readFile("test.txt");
+    }
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+       return "Hello Universe!";
+    }
+
+    @PostMapping("/hello-world")
+    public String helloWorldPost() {
+        return "You just POSTED some data!";
     }
 
 }
