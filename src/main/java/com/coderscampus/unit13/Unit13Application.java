3fa3d994556334ff280900156ab8d48f9e3772a8
Dave Naugler <davenaugler@gmail.com>
2023-12-05 18:36:55 -0700

    ✅ Learned about Component Scanning and when it is needed

--- src/main/java/com/coderscampus/unit13/Unit13Application.java
+++ src/main/java/com/coderscampus/unit13/Unit13Application.java
@@ -2,8 +2,10 @@ package com.coderscampus.unit13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.coderscampus")
 public class Unit13Application {
 
 	public static void main(String[] args) {
