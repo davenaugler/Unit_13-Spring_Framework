57c56d2bd8a2dfbd0e22528f5fde2294bdd27a2a
Dave Naugler <davenaugler@gmail.com>
2023-12-15 12:07:57 -0700

    🚀 Added more to Unit 13

--- src/main/java/com/coderscampus/unit13/Unit13Application.java
+++ src/main/java/com/coderscampus/unit13/Unit13Application.java
@@ -1,6 +1,7 @@
 package com.coderscampus.unit13;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +16,8 @@ import java.io.Reader;
 // @ComponentScan("com.coderscampus") - left in as a reminder that it exists
 public class Unit13Application {
 
-	public static void main(String[] args) throws IOException {
-		SpringApplication.run(Unit13Application.class, args);
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(Unit13Application.class, args);
 
 //        Reader in = new FileReader("recipes.csv");
 //        Iterable<CSVRecord> records = CSVFormat.RFC4180.withHeader("Cooking Minutes", "Dairy Free", "Gluten Free", "Instructions", "Preparation Minutes", "Price Per Serving", "Ready In Minutes", "Servings", "Spoonacular Score", "Title", "Vegan", "Vegetarian").parse(in);
@@ -25,17 +26,17 @@ public class Unit13Application {
 //            System.out.println(cookingMinutes);
 
         // This prints out the Cooking Instructions for the first dish.
-		Reader in = new FileReader("recipes.csv");
-		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreSurroundingSpaces().parse(in);
+        Reader in = new FileReader("recipes.csv");
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreSurroundingSpaces().parse(in);
         int count = 0;
-		for (CSVRecord record : records) {
+        for (CSVRecord record : records) {
             count++;
-            if (count == 1) {
+            if (count == 2) {
                 String instructions = record.get("Instructions");
                 System.out.println(instructions);
                 break;
             }
-
+            // Define the CSV file paths
 
 
 //			record.get(0);
@@ -45,8 +46,9 @@ public class Unit13Application {
 
 //			String lastName = record.get("Last Name");
 //			String firstName = record.get("First Name");
-		}
+        }
 
-	}
-
+    }
 }
+
+
